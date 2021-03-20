f8afc3a66dc09ec0f59d2b5295614aa0fb729d0c
Noriyuki Kurosu <norifxsc@gmail.com>
2021-03-20 18:40:49 +0900

    p80

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,15 @@
 extern void io_hlt(void);
-extern void write_mem8(int addr, int data);
+//extern void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
+	char *p;
 	int i;
 	for( i = 0x000a0000; i <= 0x000affff; i++){
 	//	write_mem8(i, i & 0x0f);
-		write_mem8(i, 13);
+	//	write_mem8(i, 13);
+		p = (char *) i;
+		*p = i & 0x0f;
 	}
 
 	for(;;){
