d86a35a980693cee245efdb32609979bd8463671
Noriyuki Kurosu <norifxsc@gmail.com>
2021-03-20 18:27:04 +0900

    p74

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,16 @@
 extern void io_hlt(void);
+extern void write_mem8(int addr, int data);
+
 void HariMain(void)
 {
-fin:
-	io_hlt();
-	goto fin;
+	int i;
+	for( i = 0x000a0000; i <= 0x000affff; i++){
+	//	write_mem8(i, i & 0x0f);
+		write_mem8(i, 13);
+	}
+
+	for(;;){
+		io_hlt();
+	}
 }
 
--- nasmfunc.asm
+++ nasmfunc.asm
@@ -2,10 +2,18 @@
 ; TAB=4
 
 section .text
-	GLOBAL	io_hlt
+		GLOBAL	io_hlt
+		GLOBAL 	write_mem8
 
 io_hlt:		; void io_hlt(void);
-	HLT
-	RLT
+		HLT
+		RLT
+
+write_mem8:		;void write_mem8(int addr, int data);
+		MOV 	ECX, [ESP+4] 	; ECX <- addr
+		MOV 	AL, [ESP+8] 	; AL <- data
+		MOV 	[ECX], AL
+		RET
+
 ; vim:ft=nasm
 
