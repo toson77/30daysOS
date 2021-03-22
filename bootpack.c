09c39eeae212334e2737092017cf0939c79b3114
Noriyuki Kurosu <norifxsc@gmail.com>
2021-03-22 14:56:18 +0900

    p103

--- bootpack.c
+++ bootpack.c
@@ -16,6 +16,7 @@ void boxfill8(unsigned char *vram,
               int x1,
               int y1);
 void init_screen(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 
 #define COL8_000000     0
 #define COL8_FF0000     1
@@ -42,12 +43,15 @@ struct BOOTINFO{
 
 void HariMain(void)
 {
-  unsigned char *vram;  // BYTE [...]用番地
-  int xsize, ysize;
   struct BOOTINFO *binfo;
+  static char font_A[16] = {
+    0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+    0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+  };
   init_palette();
   binfo = (struct BOOTINFO *) 0x0ff0;
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+  putfont8(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFFFF, font_A);
  for(;;){
     io_hlt();
   }
@@ -129,5 +133,24 @@ void init_screen(char *vram, int xsize, int ysize)
   boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
   boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 return;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+  int i;
+  char *p, d;
+  for(i = 0; i < 16; i++){
+    p = vram + (y + i) * xsize + x;
+    d = font[i];
+     if ((d & 0x80) != 0) { p[0] = c;}
+     if ((d & 0x40) != 0) { p[1] = c;}
+     if ((d & 0x20) != 0) { p[2] = c;}
+     if ((d & 0x10) != 0) { p[3] = c;}
+     if ((d & 0x08) != 0) { p[4] = c;}
+     if ((d & 0x04) != 0) { p[5] = c;}
+     if ((d & 0x02) != 0) { p[6] = c;}
+     if ((d & 0x01) != 0) { p[7] = c;}
+  }
+  return;
 }
 
