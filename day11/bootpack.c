36b560582ce9624eb6a7ed4b35ed8261521f8d14
Noriyuki <norifxsc@gmail.com>
2021-05-10 13:50:36 +0900

    harb08e

--- day11/bootpack.c
+++ day11/bootpack.c
@@ -9,7 +9,7 @@ void HariMain(void)
     char  keybuf[32], mousebuf[128];
     char s[40];
     int mx, my, i;
-    unsigned int memtotal;
+    unsigned int memtotal, count = 0;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
     struct MOUSE_DEC mdec;
     struct SHTCTL *shtctl;
@@ -40,15 +40,13 @@ void HariMain(void)
     sht_mouse = sheet_alloc(shtctl);
     sht_win = sheet_alloc(shtctl);
     buf_back  = (unsigned char *) memman_alloc_4k(memman, binfo->scrnx * binfo->scrny);
-    buf_win = (unsigned char *) memman_alloc_4k(memman, 160 * 68);
+    buf_win = (unsigned char *) memman_alloc_4k(memman, 160 * 52);
     sheet_setbuf(sht_back, buf_back, binfo->scrnx, binfo->scrny, -1); /* 透明色なし */
     sheet_setbuf(sht_mouse, buf_mouse, 16, 16, 99);
-    sheet_setbuf(sht_win, buf_win, 160, 68, -1);
+    sheet_setbuf(sht_win, buf_win, 160, 52, -1);
     init_screen8(buf_back, binfo->scrnx, binfo->scrny);
     init_mouse_cursor8(buf_mouse, 99);
-    make_window8(buf_win,160, 68, "window");
-    putfonts8_asc(buf_win, 160, 24, 28, COL8_000000, "Welcome to");
-    putfonts8_asc(buf_win, 160, 24, 44, COL8_000000, "  Haribote-OS!");
+    make_window8(buf_win,160, 52, "counter");
     sheet_slide(sht_back, 0, 0);
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
@@ -64,6 +62,12 @@ void HariMain(void)
     sheet_refresh(sht_back, 0, 0, binfo->scrnx, 48);
 
     for (;;) {
+        count++;
+        sprintf(s, "%010d", count);
+        boxfill8(buf_win, 160, COL8_C6C6C6, 40, 28, 119, 43);
+        putfonts8_asc(buf_win, 160, 40, 28, COL8_000000, s);
+        sheet_refresh(sht_win, 40, 28, 120, 44);
+
         io_cli();   /* disable interrupt*/
         if(fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
             io_stihlt();
