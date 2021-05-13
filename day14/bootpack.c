f0ec6b96e5ea3a0aec1448526279805da4257f4d
Noriyuki <norifxsc@gmail.com>
2021-05-14 01:09:43 +0900

    harib11g

--- day14/bootpack.c
+++ day14/bootpack.c
@@ -18,6 +18,14 @@ void HariMain(void)
     struct SHTCTL *shtctl;
     struct SHEET *sht_back, *sht_mouse, *sht_win;
     unsigned char *buf_back, buf_mouse[256], *buf_win;
+    static char keytable[0x54] = {
+        0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.'
+	};
 
     init_gdtidt();
     init_pic();
@@ -73,13 +81,9 @@ void HariMain(void)
     putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
 
     for (;;) {
-        /*[fix bug] if erase this line , it doesn't work */
-        sprintf(s, "%010d", timerctl.count);
-        putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 10);
-        count++;
         io_cli();
         if(fifo32_status(&fifo) == 0) {
-            io_sti();
+            io_stihlt();
         }
         else {
             i = fifo32_get(&fifo);
@@ -87,6 +91,13 @@ void HariMain(void)
             if (256 <= i && i <= 511) {
                 sprintf(s, "%02X", i - 256);
                 putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
+                if ( i < 256 + 0x54 ) {
+                    if (keytable[i - 256] != 0){
+                        s[0] = keytable[ i -256];
+                        s[1] = 0;
+                        putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 1);
+                    }
+                }
             }
             else if (512 <= i && i <= 767) {
                 if (mouse_decode(&mdec, i - 512) != 0) {
@@ -124,13 +135,9 @@ void HariMain(void)
             }
             else if ( i == 10) {
                 putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF, COL8_008484, "10[sec]", 7);
-                sprintf(s, "%010d", count);
-                /* benchmark display*/
-                putfonts8_asc_sht(sht_back, 80, 47, COL8_FFFFFF, COL8_008484, s, 10);
             }
             else if ( i == 3) {
                 putfonts8_asc_sht(sht_back, 0, 80, COL8_FFFFFF, COL8_008484, "3[sec]", 6);
-                count = 0;
             }
             else if ( i == 1) {
                 timer_init(timer3, &fifo, 0);
